18f2903b1083918fad837893286462da07fd3022
Paulo Jonas <70636527+pjonas21@users.noreply.github.com>
2024-02-16 14:14:08 +0000

    feat: lessons 2 and 3

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: "Socks",
+            description: "Socks on diferent colors, with logo of VueJS.",
+            image: "./assets/images/socks_green.jpg",
+            url: "http://www.github.com/pjonas21"
+        }
+    }
+})
