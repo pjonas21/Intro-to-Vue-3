83ca0b04d0ec6aff261ff4d17d00be3cd383ad02
pjonas21 <pjonas21@gmail.com>
2024-02-16 18:17:18 +0000

    feat: lessons 3 to 6

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: "Socks",
-            description: "Socks on diferent colors, with logo of VueJS.",
             image: "./assets/images/socks_green.jpg",
-            url: "http://www.github.com/pjonas21"
+            inventory: 100,
+            details: ['50% cotton', '30% wool', '20% poliester'],
+            variants: [
+                {id: 2234, color: 'green', image: "./assets/images/socks_green.jpg"},
+                {id: 2235, color: 'blue', image: "./assets/images/socks_blue.jpg"},
+            ],
+            
+            // code challenges
+            description: "Socks on diferent colors, with logo of VueJS.",
+            url: "http://www.github.com/pjonas21",
+            onSale: true,
+            sizes: ['pp', 'p', 'm', 'g', 'gg']
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage){
+            this.image = variantImage
+        },
+        removeItem() {
+            if(this.cart > 0) this.cart -= 1
         }
     }
+
 })
